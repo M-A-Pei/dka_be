9d5bd3f50d9d2c2157f7b7bf819892968f3c8189
Muhammad Abdullah Syafii <syafii2006@gmail.com>
2024-11-11 22:32:39 -0800

    init

---
+++ src/main/java/com/example/dka_be/DkaBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.dka_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DkaBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DkaBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dka_be/DkaBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dka_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DkaBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
